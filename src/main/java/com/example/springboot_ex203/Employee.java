01026c88c6e4c72d0c62203370b68aa07a2d3ab0
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-05-14 09:59:58 -0400

    first commit

---
+++ src/main/java/com/example/springboot_ex203/Employee.java
@@ -0,0 +1,40 @@
+package com.example.springboot_ex203;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long ssn;
+    private String dob;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/com/example/springboot_ex203/SpringbootEx203Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ex203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx203Application.class, args);
+	}
+
+}
